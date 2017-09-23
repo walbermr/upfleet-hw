9e17e3503ccfde27f5465081abe9ef808db946b3
wmr1 <wmr@cin.ufpe.br>
2017-09-23 18:18:24 -0300

    Adicionada imagens dos dataset

--- db-serial.py
+++ db-serial.py
@@ -1,6 +1,7 @@
 import setup
 import numpy as np
 import pandas as pd
+import scipy.stats as sts
 import matplotlib.pyplot as plt
 from setup import configEnvoirement, readVariables
 
@@ -16,13 +17,24 @@ def data2bytes(rpm, spd, brk):
 	return b.to_bytes(4, byteorder='big')
 
 
-def PlotVar(var):
+def plotVar(var, name):
 	plt.plot(var)
-	plt.show()
+	plt.savefig(('./figs/'+name), dpi=1000)
+	plt.cla()
+	#plt.figure(figsize = (15, 9.375)).savefig('teste.png', dpi=100)
 	return
 
 
+def smooth(y, box_pts):
+	limit = (box_pts - 1)/2
+	x = np.linspace(-limit, limit, box_pts)
+	box = sts.norm.pdf(x, 0, 2)
+	y_smooth = np.convolve(y, box, mode='same')
+	return y_smooth
+
+
 def main():
+	plt.rcParams["figure.figsize"] = [30, 22.5]
 	batch = {}				#contem todos os valores das variaveis para aquele arquivo de log
 
 	device = None 			#dispositivo pelo qual se enviara os dados
@@ -45,11 +57,17 @@ def main():
 		raise
 	
 	#envia informacoes sobre a leitura dos sensores
-	while True:
-		print("Reading file #%d: %s" %(_index, _log_names[_index]))
-		batch, _batch_size = readVariables(_log_files[_index], _variables, _log_names[_index])
+	while _index < len(_log_files):
+		log_name = _log_names[_index]
+		print("Reading file #%d: %s" %(_index, log_name))
+		batch, _batch_size = readVariables(_log_files[_index], _variables, log_name)
 		print("Batch size: %dx%d" %(_batch_size[0], _batch_size[1]))
-		PlotVar(batch["rpm"])
+
+		if(setup.SAVEFIG):
+			for i in _variables:
+				name = log_name[:len(log_name)-3]+'-'+i+'.png'
+				print("Saving %s" %(name))
+				plotVar(smooth(batch[i], 31), name)
 
 		if(device != None):
 			for i in range(0, _batch_size[1]):
@@ -72,12 +90,12 @@ def main():
 
 		_index += 1
 
-	return -1
+	return 0
 
 
 if __name__ == "__main__":
 
-	if( setup.Init(ARGS = ["debug", "serial", "tcp"]) == setup.FAIL):
+	if( setup.Init(ARGS = ["debug", "serial", "tcp", "savefigs"]) == setup.FAIL):
 		pass
 	else:
 		main()
